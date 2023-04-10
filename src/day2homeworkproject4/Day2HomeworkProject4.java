3301e86042ac699fee35e205702df3f5568063a5
sejon <sejon@LAPTOP-GI3U8FMJ.fios-router.home>
2023-04-10 14:27:45 -0400

    Second commit- implementation of getInfo 4 method

--- src/day2homeworkproject4/Day2HomeworkProject4.java
+++ src/day2homeworkproject4/Day2HomeworkProject4.java
@@ -4,5 +4,7 @@ public class Day2HomeworkProject4 {
 	public static void main(String[] args) {
 		System.out.println("Day 2 Homework Project 4");
 	}
-
+public void getInfo4( int a, int b, int c) {
+	System.out.println(a + b + c);
+}
 }
